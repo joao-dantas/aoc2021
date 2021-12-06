0c622df15dcaf96da156c1308d2bb4b069f2ddbc
João Dantas <joao.dantas@zwift.com>
2021-12-06 18:38:47 -0300

    Day 3 - Part 1. Generic.

--- day4/main.py
+++ day4/main.py
@@ -1,26 +1,29 @@
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     with open('input.txt') as f:
-        lines = f.readlines()
-        control_array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        for diagnostic in lines:
-            for idx in range(12):
-                if diagnostic[idx] == '0':
-                    control_array[idx] = control_array[idx] - 1
-                elif diagnostic[idx] == '1':
-                    control_array[idx] = control_array[idx] + 1
-        gamma_string = "0b"
-        epsilon_string = "0b"
-        print(control_array)
-        for idx in range(12):
-            if int(control_array[idx]) > 0:
-                gamma_string += "1"
-                epsilon_string += "0"
-            else:
-                gamma_string += "0"
-                epsilon_string += "1"
-        gama = int(gamma_string, 2)
-        epsilon = int(epsilon_string, 2)
-        print(gama)
-        print(epsilon)
+        control_array = []
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            line = line.strip()
+            for idx in range(len(line)):
+                if len(control_array) <= idx:
+                    control_array.append("0")
+                if line[idx] == '0':
+                    control_array[idx] = int(control_array[idx]) - 1
+                elif line[idx] == '1':
+                    control_array[idx] = int(control_array[idx]) + 1
+            gamma_string = "0b"
+            epsilon_string = "0b"
+            for idx in range(len(control_array)):
+                if int(control_array[idx]) > 0:
+                    gamma_string += "1"
+                    epsilon_string += "0"
+                else:
+                    gamma_string += "0"
+                    epsilon_string += "1"
+            gama = int(gamma_string, 2)
+            epsilon = int(epsilon_string, 2)
         print(gama * epsilon)
+
+        f.close()
