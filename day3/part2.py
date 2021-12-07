67dbb77f36a844847b30a48252f576a98ab8fe85
João Dantas <joao.dantas@zwift.com>
2021-12-07 10:42:02 -0300

    do not buffer everything in memory

--- day3/part2.py
+++ day3/part2.py
@@ -2,7 +2,7 @@ def process(list_of_numbers, evaluate_most_common, idx):
     ones_list = []
     zeros_list = []
     if len(list_of_numbers) == 1:
-        return "0b" + list_of_numbers[0]
+        return int("0b" + list_of_numbers[0], 2)
     for item in list_of_numbers:
         if item[idx] == "0":
             zeros_list.append(item)
@@ -23,28 +23,26 @@ def process(list_of_numbers, evaluate_most_common, idx):
     else:
         return process(zeros_list, evaluate_most_common, idx + 1)
 
-
 if __name__ == '__main__':
     with open('input.txt') as f:
         initial_ones_list = []
         initial_zeros_list = []
         initial_idx = 0
-        lines = f.readlines()
 
-        for line in lines:
+        while True:
+            line = f.readline()
+            if not line:
+                break
             line = line.strip()
             if line[initial_idx] == "0":
                 initial_zeros_list.append(line)
             else:
                 initial_ones_list.append(line)
         f.close()
-
         if len(initial_ones_list) >= len(initial_zeros_list):
-            oxygen_generator_rating_binary = process(initial_ones_list, True, initial_idx + 1)
-            co2_scrubber_rating_binary = process(initial_zeros_list, False, initial_idx + 1)
+            oxygen_generator_rating = process(initial_ones_list, True, initial_idx + 1)
+            co2_scrubber_rating = process(initial_zeros_list, False, initial_idx + 1)
         else:
-            oxygen_generator_rating_binary = process(initial_zeros_list, True, initial_idx + 1)
-            co2_scrubber_rating_binary = process(initial_ones_list, False, initial_idx + 1)
-        oxygen_generator_rating = int(oxygen_generator_rating_binary, 2)
-        co2_scrubber_rating = int(co2_scrubber_rating_binary, 2)
+            oxygen_generator_rating = process(initial_zeros_list, True, initial_idx + 1)
+            co2_scrubber_rating = process(initial_ones_list, False, initial_idx + 1)
         print(oxygen_generator_rating * co2_scrubber_rating)
