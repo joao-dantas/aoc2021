206d84599f905d757d89b5e6db7b776003d8d9db
João Dantas <jlnery@gmail.com>
2021-12-11 10:17:11 -0300

    Better code for day 4 not using in memory to save all data.

--- day4/part1.py
+++ day4/part1.py
@@ -1,55 +1,73 @@
-def check_winner(board):
-    for line in board:
-        if all(num == -1 for (num) in line) is True:
-            return True
-    for i in range(5):
-        if board[0][i] == -1 and board[1][i] == -1 and board[2][i] == -1 and board[3][i] == -1 and board[4][i] == -1:
-            return True
-    return False
+class BingoParticipant:
+    board = {}
+    rounds_to_win = 0
+    win_num = 0
+    already_won = False
+
+    def __init__(self):
+        self.rounds_to_win = 0
+        self.win_num = 0
+        self.board = {}
 
 
-def get_sum_unmarked(board):
-    total_unmarked = 0
-    for line in board:
-        filtered_line = filter(lambda x: x > 0, line)
-        for item in filtered_line:
-            total_unmarked += int(item)
-    return total_unmarked
+def check_participant(participant_checked):
+    if participant_checked.board:
+        for current_round, sel_num in enumerate(selected_numbers):
+            if participant_checked.already_won is False:
+                for key in participant_checked.board:
+                    for i, x in enumerate(participant_checked.board[key]):
+                        if x == sel_num:
+                            participant_checked.board[key][i] = participant_checked.board[key][i] * (-1)
+                    if all(elem < 0 for elem in participant_checked.board[key]):
+                        participant_checked.already_won = True
+                        participant_checked.rounds_to_win = current_round
+                        participant_checked.win_num = sel_num
+            else:
+                continue
+    return participant_checked
 
-
-def process_boards(bingo_boards, number_selected):
-    for board in bingo_boards:
-        for line in board:
-            for idx, num in enumerate(line):
-                if num == number_selected:
-                    line[idx] = -1
-                    # The check winner could be done while we're marking the board to avoid go over the board again
-                    if check_winner(board):
-                        total_unmarked = get_sum_unmarked(board)
-                        print number_selected
-                        print total_unmarked
-                        print int(number_selected) * total_unmarked
-                        return True
-    return False
 
 if __name__ == '__main__':
-    with open('input.txt') as f:
-        lines = f.readlines()
-        f.close()
-        new_board = []
-        boards = []
+    with open('example_input.txt') as f:
+        selected_numbers = {}
         current_read_index = 0
-        while current_read_index < len(lines):
-            if current_read_index == 0:
-                selected_numbers = lines[current_read_index].strip().split(",")
-            elif lines[current_read_index] == "\n":
-                boards.append(new_board)
-                new_board = []
-            else:
-                new_board.append(lines[current_read_index].strip().replace("  ", " ").split(" "))
-            current_read_index += 1
-        boards.append(new_board)
-
-        for selected in selected_numbers:
-            if process_boards(boards, selected) is True:
+        current_board_line = 0
+        participant = BingoParticipant()
+        winner = BingoParticipant()
+        best_rounds_to_win = 99999
+        while True:
+            file_line = f.readline()
+            if not file_line:
                 break
+            file_line = file_line.strip()
+            # print file_line
+            if current_read_index == 0:
+                selected_numbers = map(int, file_line.split(','))
+            elif file_line != '':
+                line = map(int, file_line.replace("  ", " ").split(" "))
+                participant.board["l" + str(current_board_line)] = line
+                for col_idx in range(5):
+                    if "c" + str(col_idx) not in participant.board:
+                        participant.board["c" + str(col_idx)] = [
+                            participant.board["l" + str(current_board_line)][col_idx]]
+                    else:
+                        participant.board["c" + str(col_idx)].append(
+                            participant.board["l" + str(current_board_line)][col_idx])
+                current_board_line += 1
+                if len(participant.board) == 10:
+                    participant = check_participant(participant)
+                    if participant.already_won and participant.rounds_to_win < best_rounds_to_win:
+                        winner = participant
+                        best_rounds_to_win = participant.rounds_to_win
+            else:
+                participant = BingoParticipant()
+                current_board_line = 0
+            current_read_index += 1
+
+        total_unmarked = 0
+        for key in winner.board:
+            for num in winner.board[key]:
+                if num >= 0:
+                    total_unmarked += num
+        print (total_unmarked / 2) * winner.win_num
+        f.close()
--- day4/part2.py
+++ day4/part2.py
@@ -1,34 +1,73 @@
-def process(list_of_numbers, evaluate_most_common, idx):
-    ones_list = []
-    zeros_list = []
-    if len(list_of_numbers) == 1:
-        return int("0b" + list_of_numbers[0], 2)
-    for item in list_of_numbers:
-        if item[idx] == "0":
-            zeros_list.append(item)
-        else:
-            ones_list.append(item)
+class BingoParticipant:
+    board = {}
+    rounds_to_win = 0
+    win_num = 0
+    already_won = False
 
-    if len(ones_list) > len(zeros_list):
-        if evaluate_most_common:
-            return process(ones_list, evaluate_most_common, idx + 1)
-        else:
-            return process(zeros_list, evaluate_most_common, idx + 1)
-    elif len(ones_list) < len(zeros_list):
-        if evaluate_most_common:
-            return process(zeros_list, evaluate_most_common, idx + 1)
-        else:
-            return process(ones_list, evaluate_most_common, idx + 1)
-    elif evaluate_most_common:
-        return process(ones_list, evaluate_most_common, idx + 1)
-    else:
-        return process(zeros_list, evaluate_most_common, idx + 1)
+    def __init__(self):
+        self.rounds_to_win = 0
+        self.win_num = 0
+        self.board = {}
+
+
+def check_participant(participant_checked):
+    if participant_checked.board:
+        for current_round, sel_num in enumerate(selected_numbers):
+            if participant_checked.already_won is False:
+                for k in participant_checked.board:
+                    for i, x in enumerate(participant_checked.board[k]):
+                        if x == sel_num:
+                            participant_checked.board[k][i] = -1
+                    if all(elem < 0 for elem in participant_checked.board[k]):
+                        participant_checked.already_won = True
+                        participant_checked.rounds_to_win = current_round
+                        participant_checked.win_num = sel_num
+            else:
+                continue
+    return participant_checked
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-        lines = f.readlines()
+        selected_numbers = {}
+        current_read_index = 0
+        current_board_line = 0
+        participant = BingoParticipant()
+        loser = BingoParticipant()
+        worst_round_to_win = 0
+        while True:
+            file_line = f.readline()
+            if not file_line:
+                break
+            file_line = file_line.strip()
+            # print file_line
+            if current_read_index == 0:
+                selected_numbers = map(int, file_line.split(','))
+            elif file_line != '':
+                line = map(int, file_line.replace("  ", " ").split(" "))
+                participant.board["l" + str(current_board_line)] = line
+                for col_idx in range(5):
+                    if "c" + str(col_idx) not in participant.board:
+                        participant.board["c" + str(col_idx)] = [
+                            participant.board["l" + str(current_board_line)][col_idx]]
+                    else:
+                        participant.board["c" + str(col_idx)].append(
+                            participant.board["l" + str(current_board_line)][col_idx])
+                current_board_line += 1
+                if len(participant.board) == 10:
+                    participant = check_participant(participant)
+                    if participant.already_won and participant.rounds_to_win > worst_round_to_win:
+                        loser = participant
+                        worst_round_to_win = participant.rounds_to_win
+            else:
+                participant = BingoParticipant()
+                current_board_line = 0
+            current_read_index += 1
+
+        total_unmarked = 0
+        for key in loser.board:
+            for num in loser.board[key]:
+                if num >= 0:
+                    total_unmarked += num
+        print (total_unmarked / 2) * loser.win_num
         f.close()
-    oxygen_generator_rating = process(lines, True, 0)
-    co2_scrubber_rating = process(lines, False, 0)
-    print(oxygen_generator_rating * co2_scrubber_rating)
