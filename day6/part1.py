19fa940582e5c04cfeef99a851ef663e663b9156
João Dantas <jlnery@gmail.com>
2021-12-17 22:23:22 -0300

    Part 1 - Day 7

--- day6/part1.py
+++ day6/part1.py
@@ -1,26 +1,16 @@
-from functools import cache
-
-
-@cache
-def process_lanternfish_lifecycle(days_left, current_age):
-    family = 1
-    for d in range(days_left, 0, -1):
-        if current_age == 0:
-            family += process_lanternfish_lifecycle(d, 9)
-            current_age = 6
-        else:
-            current_age -= 1
-
-    return family
-
+import statistics
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         lines = f.readlines()
-        days = 256
-        list_of_ages = map(lambda x: int(x), lines[0].split(","))
-        list_of_lanterfishes = 0
-        for age in list_of_ages:
-            list_of_lanterfishes += process_lanternfish_lifecycle(days, age)
-        print("Total: " + str(list_of_lanterfishes))
+        list_of_crabs_position = list(map(lambda x: int(x), lines[0].split(",")))
+        list_of_crabs_position.sort()
+        less_steps = 999999
+        for idx_i, pos1 in enumerate(list_of_crabs_position):
+            total_steps = 0
+            for idx_j, pos2 in enumerate(list_of_crabs_position):
+                total_steps += abs(pos1 - pos2)
+            if total_steps < less_steps:
+                less_steps = total_steps
+        print(less_steps)
     f.close()
